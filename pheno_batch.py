7cb4611bbff4c748925835ca5b1dddee4b2d1105
Yuankai He <yh464@cam.ac.uk>
2026-02-26 16:26:51 +0000

    bug fix

--- pheno_batch.py
+++ pheno_batch.py
@@ -15,13 +15,13 @@ def qc(input_args):
 
   skip = True
   try:
-    tmp = pd.read_table(f'{args.out}/global_graph/{subj}.txt')
+    tmp = pd.read_table(f'{args.out}/global_graph/{subj}.txt', index_col = 0)
     if tmp.shape[0] != 17: skip = False
-    tmp = pd.read_table(f'{args.out}/global_asym/{subj}.txt')
+    tmp = pd.read_table(f'{args.out}/global_asym/{subj}.txt', index_col = 0)
     if tmp.shape[0] != 17: skip = False
-    tmp = pd.read_table(f'{args.out}/local/{subj}.txt')
+    tmp = pd.read_table(f'{args.out}/local/{subj}.txt', index_col = 0)
     if tmp.shape[0] != nroi or tmp.shape[1] != 7: skip = False
-    tmp = pd.read_table(f'{args.out}/local_asym/{subj}.txt')
+    tmp = pd.read_table(f'{args.out}/local_asym/{subj}.txt', index_col = 0)
     if tmp.shape[0] != nroi/2 or tmp.shape[1] != 21: skip = False
   except: skip = False
   return True, skip
